3f520059bbcb2907ec52b065a92a0928894b653c
shinytang6 <1074461480@qq.com>
2017-11-25 19:07:13 +0800

    add some wincap func

--- sniffer.cpp
+++ sniffer.cpp
@@ -1,9 +1,5 @@
 #include "sniffer.h"
 #include <QDebug>
-#include <pcap.h>
-    #define WPCAP
-    #define HAVE_REMOTE
-    #include <remote-ext.h>
 Sniffer::Sniffer()
 {
 
@@ -14,14 +10,90 @@ Sniffer::~Sniffer()
 
 }
 
-void Sniffer::findAllDevs(){
-    if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL, &alldevs, errbuf) == -1) {
+void Sniffer::findAllDevs(char *szFlag){
+    if (pcap_findalldevs_ex(szFlag, NULL, &alldevs, errbuf) == -1) {
          qDebug() << "error " ;
      }
 
     /* 打印列表 */
     for(dev= alldevs; dev != NULL; dev= dev->next)
-    {
+    {       iNetDevsNum++;
             qDebug() << dev->name << ":" <<dev->description;
+    }
+}
+
+bool Sniffer::openNetDev(int devNum){
+    int i;
+    if(devNum < 1 || devNum > iNetDevsNum)
+        {
+            qDebug() << "\nInterface number out of range.\n";
+            /* 释放设备列表 */
+            freeDevsMem();
+            return -1;
+        }
+        /* 跳转到选中的适配器 */
+        for(dev=alldevs, i=0; i< devNum-1 ;dev=dev->next, i++);
+
+        /* 打开设备 */
+        if ( (adhandle= pcap_open(dev->name,          // 设备名
+                                  65536,            // 65535保证能捕获到不同数据链路层上的每个数据包的全部内容
+                                  PCAP_OPENFLAG_PROMISCUOUS,    // 混杂模式
+                                  1000,             // 读取超时时间
+                                  NULL,             // 远程机器验证
+                                  errbuf            // 错误缓冲池
+                                  ) ) == NULL)
+        {
+            qDebug() << "\nInterface number out of range.\n";"\nUnable to open the adapter. %s is not supported by WinPcap\n";
+            /* 释放设备列表 */
+            freeDevsMem();
+            return -1;
+        }
+}
+
+bool Sniffer::setDevsFilter(char *szFilter)
+{
+    /* 检查数据链路层，为了简单，我们只考虑以太网 */
+        if(pcap_datalink(adhandle) != DLT_EN10MB)
+        {
+            fprintf(stderr,"\nThis program works only on Ethernet networks.\n");
+            /* 释放设备列表 */
+            freeDevsMem();
+            return -1;
+        }
+        u_int	netmask;
+
+        if(dev->addresses != NULL)
+            /* 获得接口第一个地址的掩码 */
+            netmask=((struct sockaddr_in *)(dev->addresses->netmask))->sin_addr.S_un.S_addr;
+        else
+            /* 如果接口没有地址，那么我们假设一个C类的掩码 */
+            netmask=0xffffff;
+
+
+        //编译过滤器
+        if (pcap_compile(adhandle, &fcode, szFilter, 1, netmask) <0 )
+        {
+            freeDevsMem();
+            return -1;
+        }
+
+        //设置过滤器
+        if (pcap_setfilter(adhandle, &fcode)<0)
+        {
+            /* 释放设备列表 */
+            freeDevsMem();
+            return -1;
+        }
+}
+
+void Sniffer::captureByCallBack(snifferCB func){
+    /* 开始捕捉 */
+    pcap_loop(adhandle, 0, func, NULL);
+}
+
+void Sniffer::freeDevsMem(){
+    if(alldevs){
+        pcap_freealldevs(alldevs);
+        alldevs = NULL;
     }
 }
--- sniffer.h
+++ sniffer.h
@@ -1,14 +1,25 @@
 #ifndef SNIFFER_H
 #define SNIFFER_H
 #include "pcap.h"
+#define WPCAP
+#define HAVE_REMOTE
+#include <remote-ext.h>
 
 class Sniffer
 {
 public:
     Sniffer();
     ~Sniffer();
+    typedef void (*snifferCB)(u_char *, const struct pcap_pkthdr *, const u_char *);
 
-    void findAllDevs();			// 获取全部网络设备列表信息
+    void findAllDevs(char *szFlag = PCAP_SRC_IF_STRING);			// 获取全部网络设备列表信息
+    void freeDevsMem();											// 释放网络设备信息占据的堆内存
+    void openNetDev(char *szDevName);								// 根据名称打开网络设备
+    bool openNetDev(int iDevNum);									// 根据序号打开网络设备
+    bool setDevsFilter(char *szFilter);                             // 对当前打开设备设置过滤器
+    int	 captureOnce();												// 捕获一次网络数据包
+    void captureByCallBack(snifferCB func);						// 以回调函数方式捕获数据
+
     /*void createDevsStr(char *source, char *szFileName);				// WinPcap语法创建一个源字符串
     void freeNetDevsMem();											// 释放网络设备信息占据的堆内存
     bool openNetDev(char *szDevName);								// 根据名称打开网络设备
@@ -29,6 +40,8 @@ protected:
     pcap_if_t *alldevs;
     pcap_if_t *dev;
     char errbuf[PCAP_ERRBUF_SIZE];
-
+    int				iNetDevsNum;					// 网络设备数量
+    struct bpf_program fcode;
+    pcap_t			*adhandle;						// 当前打开的设备句柄（指针）
 };
 #endif // SNIFFER_H
